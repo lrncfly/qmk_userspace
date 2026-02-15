771b8543f20f28b9a1319374567e3963ac04e8f8
Laurence Foley <lrncfly@users.noreply.github.com>
2026-02-15 12:11:31 +0000

    lto enable

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/config.h
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/config.h
@@ -26,6 +26,8 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 #define ENCODER_RESOLUTION 4
 
+#define NO_MUSIC_MODE
+
 #define RGB_MATRIX_SLEEP // turn off effects when suspended
 #define SPLIT_LAYER_STATE_ENABLE
 #define SPLIT_LED_STATE_ENABLE
