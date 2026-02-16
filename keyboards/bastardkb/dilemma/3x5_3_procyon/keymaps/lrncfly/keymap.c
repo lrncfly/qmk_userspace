1bcfd58843c7449c18cabad1771396b82737f1e3
Laurence Foley <lrncfly@users.noreply.github.com>
2026-02-16 09:06:22 +0000

    align -_

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/lrncfly/keymap.c
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL, XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI,
      KC_DOT,    KC_1,    KC_2,    KC_3, KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                       KC_DOT, KC_MINS,    KC_0, XXXXXXX, _______, XXXXXXX
+                         KC_0,  KC_DOT, KC_MINS, XXXXXXX, _______, XXXXXXX
   ),
 
 /**
